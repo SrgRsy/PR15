3d7717faffd7869760526cf95bdd2159524ec59a
Рясный Сергей Сергеевич <ss_ryasniy@bbgroup.local>
2019-12-26 03:48:00 +0300

    first commit

---
+++ middlewares/auth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const AuthorizError = require('../errors/authoriz-err');
+
+
+
+module.exports = (req, res, next) => {
+  const { NODE_ENV, JWT_SECRET } = process.env;
+
+
+  const extractBearerToken = (header) => {
+    return header.replace('Bearer ', '');
+  };
+  const { authorization } = req.headers;
+  console.log(authorization);
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return  next(new AuthorizError('Авторизуйтесь'));
+  }
+
+  const token = extractBearerToken(authorization);
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (next) {
+    return next(new AuthorizError('Авторизуйтесь'));
+  }
+  req.user = payload;
+
+  next();
+};
+
+
+
